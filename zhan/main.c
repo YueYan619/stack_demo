b1a54e83801083f1292b0ed6e87ce6ab960d577b
YueYan910619 <448953616@qq.com>
2016-03-11 20:02:25 +0800

    Initial Commit

---
+++ zhan/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  zhan
+//
+//  Created by 岳言 on 16/3/11.
+//  Copyright © 2016年 岳言. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
